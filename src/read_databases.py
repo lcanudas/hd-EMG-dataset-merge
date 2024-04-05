0a446a7c44b1d262404ab64ec1fb36936bb4e431
nicbmoura <nicolemoura95@gmail.com>
2024-04-05 14:03:10 -0300

    seeds

--- src/read_databases.py
+++ src/read_databases.py
@@ -15,64 +15,42 @@ import yaml
 import sys
 import os
 
+print(sys.path)
+print(os.environ.get('PYTHONPATH'))
+print(os.environ.items)
+config = yaml.safe_load(open('params.yaml'))
 
-
-
-def read_database1(time,h, w, subj, type, task, intensity, trial,  pathcwd=''):
+def read_database1(time,h, w, subj, type, task, intensity, trial):
     """_summary_
 
     Args:
         time (_type_): _description_
         h (_type_): _description_
         w (_type_): _description_
-        subj (int): _description_
+        subj (int): from 1 to 7
         type (string): can be hdEMG or fwEMG
         task (string): can be baseline, DFMVC, ISOK, ISOT, PFMVC, RAH, SINE
-        intensity (int):  can be 30, 90 300, 25, 50, None. Note that  these intensities are not available for all tasks.
+        intensity (int):  can be 30, 90, 300, 25, 50, None, 10. Note that  these intensities are not available for all tasks.
         trial (int): can be 1, 2, 3, None
     """
-    config = yaml.safe_load(open(pathcwd+'params.yaml'))
+
     path = config['data_path']['seeds']
     subj_string = 'S'+str(subj).zfill(2)
     if intensity != None:
-        intnsity_string = str(intensity)
+        intensity_string = str(intensity)
     else:
         intensity_string = ''
     if trial != None:
         trial_string = '_'+str(trial)
     else:
         trial_string = ''
-    db = pd.read_csv(pathcwd+path+'/'+type+'/'+subj_string+'/'+subj_string+'_'+task+intensity_string+trial_string+'.csv')
+    db = pd.read_csv(path+'/'+type+'/'+subj_string+'/'+subj_string+'_'+task+intensity_string+trial_string+'.csv')
     img = db.loc[time,'MA1':'MN9'].values.reshape(9,14)
     resized = resize(img, (h,w), anti_aliasing=True)
 
     return resized
 
-def read_database3(time,h, w, subj, type, task, intensity, trial, pathcwd=''):
-    """_summary_
-
-    Args:
-        time (_type_): _description_
-        h (_type_): _description_
-        w (_type_): _description_
-        subj (_type_): _description_
-        type (_type_): _description_
-        task (_type_): _description_
-        intensity (_type_): _description_
-        trial (_type_): _description_
-        pathcwd (str, optional): _description_. Defaults to ''.
-
-    Returns:
-        _type_: _description_
-    """    
-    
-    plt.imshow(db2['emg_extensors'][t])
-
-
-    return resized
-
-
-def read_database4(time, h, w, subj, type, task, intensity, trial,  pathcwd=''):
+def read_database4(time,h, w, subj, type, task, intensity, trial):
     """_summary_
 
     Args:
@@ -85,51 +63,44 @@ def read_database4(time, h, w, subj, type, task, intensity, trial,  pathcwd=''):
         intensity (int): can be 10, 30, 50
         trial (_type_): None
     """   
-    config = yaml.safe_load(open(pathcwd+'params.yaml'))
     path = config['data_path']['dado4']
     fs = config['database_params']['dado4']['fs']
-    n_channels_file = pd.read_csv(pathcwd+path+'nchannels.txt', sep='\s+', index_col='subject')
+    n_channels = pd.read_csv(path+'nchannels.txt')
     
     type_dict = {'biceps':'bb', 'forearm':'fa', 'torque':'torque', 'triceps':'tb'}
-    nrows_dict = {'biceps':8, 'forearm':8, 'torque':1, 'triceps':6}
     subj_string = 's'+str(subj)
     intensity_string = str(intensity)
+    return n_channels
 
-    n_channels = n_channels_file.loc[subj_string, type]
+def read_database3(time, h, w, subj, exp, session, X, Y)
+    """_summary_
 
-    file = open(pathcwd+path+subj_string+'/'+type+'/'+subj_string+'_'+task+intensity_string+'_'+type_dict[type]+'.bin',"rb")
-    dado = np.fromfile(file, dtype='<d')
-    if type != 'torque':
-        dado = dado.reshape(n_channels,-1)
-        dado = dado.T
-        dado = dado[time,:].reshape(nrows_dict[type],-1)
-        resized = resize(dado, (h,w), anti_aliasing=True)
-    else:
-        resized = dado[time]
+    Args:
+        time (int):
+        h (int):
+        w (int):
+        subj (int): from 1 to 25
+        exp (int): from 1 to ?
+        session (int): from 1 to 3
+        X (int): from 1 to 13
+        Y (int): from 1 to 6
+    """
+
+    path = config['data_path']['seeds']
+
+    subj_string = 'subj'+str(subj).zfill(2)
+    exp = str(exp).zfill(2)
+    session = 'Sess'+str(session)
+    X = str(X).zfill(2)
+    Y = str(Y).zfill(2)
+
+
+    seeds = loadmat(path+'\ '+subj_string+'\ '+'detop_exp'+exp+'_'+subj_string+'_'+session+'_'+X+'_'+Y+'.mat')
+    # as aspas em relação a: \
+
+    emg = seeds[emg][:126,:]
+    resized = plt.imshow(emg[:,time].reshape(h,w))
+    # função tirada do 'funções_imagem.ipynb'
+    # não sei se o 'time' está bem localizado
 
     return resized
-
-def get_length_database4(subj, type, task, intensity, trial,  pathcwd=''):
-
-    config = yaml.safe_load(open(pathcwd+'params.yaml'))
-    path = config['data_path']['dado4']
-    fs = config['database_params']['dado4']['fs']
-    n_channels_file = pd.read_csv(pathcwd+path+'nchannels.txt', sep='\s+', index_col='subject')
-    
-    type_dict = {'biceps':'bb', 'forearm':'fa', 'torque':'torque', 'triceps':'tb'}
-    nrows_dict = {'biceps':8, 'forearm':8, 'torque':1, 'triceps':6}
-    subj_string = 's'+str(subj)
-    intensity_string = str(intensity)
-
-    n_channels = n_channels_file.loc[subj_string, type]
-
-    file = open(pathcwd+path+subj_string+'/'+type+'/'+subj_string+'_'+task+intensity_string+'_'+type_dict[type]+'.bin',"rb")
-    dado = np.fromfile(file, dtype='<d')
-    if type != 'torque':
-        dado = dado.reshape(n_channels,-1)
-        dado = dado.T
-        length = len(dado)
-    else:
-        length = len(dado)
-
-    return length
